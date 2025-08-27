58073fd96a2cff9ec11e7b84b34d41b8d764b01c
Lukas Baumeister <baumeister.luke@web.de>
2025-08-27 10:55:27 +0200

    Course Repository and Service was added.

---
+++ src/main/java/com/example/studentorganizer/Course/Course.java
@@ -0,0 +1,85 @@
+package com.example.studentorganizer.Course;
+
+import com.example.studentorganizer.Professor.Professor;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Professor> professor;
+
+    private Integer credits;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+
+
+    public Course() {}
+
+    public Course(String name, String description, Integer credits, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.description = description;
+        this.credits = credits;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Integer credits) {
+        this.credits = credits;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ src/main/java/com/example/studentorganizer/Course/CourseService.java
@@ -0,0 +1,33 @@
+package com.example.studentorganizer.Course;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CourseService {
+
+    private final CourseRepository courserepository;
+
+
+    @Autowired
+    public CourseService(CourseRepository courserepository) {
+        this.courserepository = courserepository;
+    }
+
+    public Course addCourse(Course course) {
+        courserepository.save(course);
+        return course;
+    }
+
+    public List<Course> getAllCourses() {
+        return courserepository.findAll();
+    }
+
+    @Transactional
+    public void deleteCourse(Course course) {
+        courserepository.delete(course);
+    }
+}
